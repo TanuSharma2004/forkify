c95d3ee1c6ec49adbf036e61a9365bed50178972
Tanu Sharma <tanubindusharma1@gmail.com>
2026-06-24 19:03:56 +0530

    Added files

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.jonas.io/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'df167888-bd75-4118-bd67-4f98cb787498';
+export const MODAL_CLOSE_SEC = 2.5;
